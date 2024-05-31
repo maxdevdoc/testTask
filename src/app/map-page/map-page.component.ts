b0cf9fea271f6cfdad223c30fb12eb7ff9e31be0
maxdevdoc <max.dev.doc@gmail.com>
2024-05-31 07:11:36 +0300

    delete comment

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -27,18 +27,7 @@ export class MapComponent implements OnInit {
 
   ngOnInit(): void {
     this.map = this.mapService.getMap();
-    // (mapboxgl as any).accessToken =
-    //   "pk.eyJ1IjoibWFyb29uZWRpb25lIiwiYSI6ImNqdmp0MzB1azBpcDAzem1naHZwMjNndGIifQ.65nvvRg9QeFUV2c6b9W4Vw";
-
-    // this.map = new mapboxgl.Map({
-    //   container: "map",
-    //   style: "mapbox://styles/mapbox/streets-v12",
-    //   center: [-87.61694, 41.86625],
-    //   zoom: 15.99,
-    //   pitch: 40,
-    //   bearing: 20,
-    //   antialias: true,
-    // });
+ 
 
     this.map.on("load", () => {
       this.map?.addSource("drawn-polygons", {
